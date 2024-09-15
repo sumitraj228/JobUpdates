5533a1380a5eeca982e6a091391529ffca1e5204
sumit raj <sumitrajsingh228@gmail.com>
2024-09-15 23:47:02 +0530

    create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+import Jobs from './pages/Jobs'
 
 export default function App() {
   return (
-    <div>
-      <h1 className=' text-red-500'>App</h1>
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<Signup/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/jobs' element={<Jobs/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
