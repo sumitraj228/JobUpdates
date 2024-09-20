7d184cd8fae6bfc0e34608826c35386d8e5f5f49
sumit raj <sumitrajsingh228@gmail.com>
2024-09-20 10:51:11 +0530

    signout functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react'
 import {Sidebar} from 'flowbite-react'
 import {HiUser, HiArrowSmRight} from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function DashSidebar() {
+  const dispatch=useDispatch();
     const location = useLocation()
     const [tab, setTab]= useState('');
       useEffect(()=>{
@@ -14,6 +17,26 @@ export default function DashSidebar() {
           setTab(tabFromUrl);
         }
       },[location.search]);
+
+
+
+      const handleSignout = async () => {
+        try {
+          const res = await fetch('/api/user/signout',{
+            method: 'POST',
+          });
+          const data =await res.json();
+  
+          if(!res.ok){
+            console.log(data.message)
+          }
+          else{
+            dispatch(signoutSuccess());
+          }
+        } catch (error) {
+          console.log(error.message)
+        }} 
+
     
   return (
     <Sidebar className='w-full md:w-56'>
@@ -23,7 +46,7 @@ export default function DashSidebar() {
                 <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>Profile</Sidebar.Item>
                 </Link>
 
-                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
+                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >Sign Out</Sidebar.Item>
 
             </Sidebar.ItemGroup>
         </Sidebar.Items>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -51,9 +51,14 @@ const userSlice =createSlice({
             state.loading = false;
             state.error=action.payload;
         },
+        signoutSuccess:(state)=>{
+            state.currentUser =null;
+            state.loading = false;
+            state.error=null;
+        },
     }
 });
 
-export const {signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure}=userSlice.actions;
+export const {signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess}=userSlice.actions;
 
 export default userSlice.reducer;
